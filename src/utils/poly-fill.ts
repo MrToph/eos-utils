31f15153c2ebc0db281d33fed0b4640669462b48
MrToph <christoph.michel.1992@gmail.com>
2020-03-24 14:19:10 +0400

    Fix window.TextEncoder bug for test-environments that are not set to node

--- src/utils/poly-fill.ts
+++ src/utils/poly-fill.ts
@@ -1,7 +1,15 @@
 const isBrowser = () => typeof window !== `undefined`;
 
 /* eslint-disable no-undef, global-require */
-const textEncoder = isBrowser() ? new window.TextEncoder() : new (require(`util`).TextEncoder)();
-const textDecoder = isBrowser() ? new window.TextDecoder() : new (require(`util`).TextDecoder)();
+// even if window exists, we cannot be sure if TextEncoder exists
+// for example when testing with js-dom
+const textEncoder =
+  isBrowser() && window.TextEncoder
+    ? new window.TextEncoder()
+    : new (require(`util`).TextEncoder)();
+const textDecoder =
+  isBrowser() && window.TextDecoder
+    ? new window.TextDecoder()
+    : new (require(`util`).TextDecoder)();
 
 export { textEncoder, textDecoder };
